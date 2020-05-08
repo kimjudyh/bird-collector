7ada6a94059c1dc01c4bd64360079d15ad131402
Judy <kimjudyh@gmail.com>
2020-05-08 15:37:20 -0700

    applied bootstrap styling, added navbar, added birds route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('birds/', views.birds_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def home(request):
     template = 'index.html'
     return render(request, template)
+
+def birds_index(request):
+    template = 'birds/index.html'
+    # context = {'birds': birds}
+    return render(request, template)
