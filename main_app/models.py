100c0bef72d3a7770afb6e66aa02a02bb8382b3a
Judy <kimjudyh@gmail.com>
2020-05-12 09:18:55 -0700

    added Sighting model, linked to Bird model

--- main_app/models.py
+++ main_app/models.py
@@ -8,3 +8,20 @@ class Bird(models.Model):
     
     def __str__(self):
         return self.name
+
+
+# one-to-many relationship model
+class Sighting(models.Model):
+    date = models.DateField('Date Seen')
+    location = models.CharField(max_length=100)
+    notes = models.TextField(max_length=300)
+
+    # define relationship to Bird using foreign key
+    bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Sighting on {self.date}"
+
+    class Meta:
+        # sort by most recent date
+        ordering = ['-date']
